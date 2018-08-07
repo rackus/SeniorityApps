af5eb3929b6dc51dcc0358915b84ea9f7ccbc0a9
Rackus <rak.wojciech@gmail.com>
2018-08-07 20:40:34 +0200

    Add project files.

---
+++ SeniorityConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeniorityConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
